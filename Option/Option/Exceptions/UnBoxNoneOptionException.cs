15be6296be4e8513a95f572b1f851c86a336214e
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2017-09-29 10:56:52 +0300

    Option

---
+++ Option/Option/Exceptions/UnBoxNoneOptionException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Option.Exceptions
+{   
+    [Serializable]
+    public class UnBoxNoneOptionException : Exception
+    {
+        public UnBoxNoneOptionException() { }
+        public UnBoxNoneOptionException(string message) : base(message) { }
+        public UnBoxNoneOptionException(string message, Exception inner) : 
+        
+            base(message, inner) { }
+        
+        protected UnBoxNoneOptionException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ Option/Option/Option.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Option.Exceptions;
+
+namespace Option
+{
+    public class Option<T>
+    {
+        private Option(T val, bool isSome = true)
+        {
+            _val = val;
+            _isSome = isSome;
+        }
+
+        private Option() :
+            this(default(T), false)
+        {}
+
+        private readonly T _val;
+        private readonly bool _isSome;
+
+        public static Option<T> None() => new Option<T>();
+
+        public static Option<T> Some(T val) => new Option<T>(val);
+
+        public bool IsSome() => _isSome;
+
+        public bool IsNone() => !_isSome;
+
+        public T Value()
+        {
+            if (IsNone())
+            {
+                throw new UnBoxNoneOptionException();
+            }
+            return _val;
+        }
+
+        public Option<U> Map<U>(Func<T, U> f) => _isSome ? Option<U>.Some(f(_val)) : Option<U>.None();
+
+        public static Option<T> Flatten(Option<Option<T>> option)
+            => option.IsSome() && option.Value().IsSome() ? Some(option.Value().Value()) : None();
+    }
+}
