5dd34ee50d8f09fae576233607276f49ea7f6038
bzik <alexander.bzikadze@gmail.com>
2017-12-02 17:39:14 +0300

    Tests added.

---
+++ Mini-Roguelike/Mini-Roguelike/Tests/CharGameMapReaderTests.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Mini_Roguelike.Tests
+{
+    [TestFixture]
+    public class CharGameMapReaderTests
+    {
+        [Test]
+        public void NoFailWhenNotFoundWhenConstructor()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            // The result not neaded, only that does not fails
+            Assert.DoesNotThrow(() => 
+                new CharGameMapReader(FileFromTestsReader.GetFile("CharGameMapReaderFailWhenNotFoundTest.txt")));
+        }
+        
+        [Test]
+        public void FailWhenNotFoundWhenRead()
+        {
+            Assert.Throws<FileNotFoundException>(() => 
+                new CharGameMapReader(
+                    FileFromTestsReader.GetFile("CharGameMapReaderFailWhenNotFoundTest.txt")).GetMap());
+        }
+
+        [Test]
+        public void ReadSuccessEmpty()
+        {
+            Assert.NotNull(new CharGameMapReader(
+                FileFromTestsReader.GetFile("CharGameMapReaderEmptyTest.txt")).GetMap());
+        }
+
+        [Test]
+        public void ReadSuccessRandom()
+        {
+            Assert.NotNull(new CharGameMapReader(
+                FileFromTestsReader.GetFile("CharGameMapReaderRandomTest.txt")).GetMap());
+        }
+
+        [Test]
+        public void ReadCorrectRandom()
+        {
+            var gameMap = new CharGameMapReader(
+                FileFromTestsReader.GetFile("CharGameMapReaderRandomTest.txt")).GetMap();
+            for (var i = 0; i < 3; ++i)
+            {
+                for (var j = 0; j < 3; ++j)
+                {
+                    if (i == 1 && j == 1)
+                    {
+                        Assert.IsTrue(gameMap.CanGo(i, j));
+                    }
+                    else
+                    {
+                        Assert.IsFalse(gameMap.CanGo(i, j));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Mini-Roguelike/Mini-Roguelike/Tests/GameTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Mini_Roguelike.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {   
+        [Test]
+        public void CorrectConstructor()
+        {
+            Assert.DoesNotThrow(() => 
+                new Game(new CharGameMapReader(FileFromTestsReader.GetFile("GameBoundedMapTest.txt"))));
+        }
+        
+        [Test]
+        public void CorrectPlayerSet()
+        {
+            var game = new Game(
+                new CharGameMapReader(FileFromTestsReader.GetFile("GameBoundedMapTest.txt")));
+            game.MapChanged += (sender, args) =>
+            {
+                Assert.AreEqual(args.Player.X, 1);
+                Assert.AreEqual(args.Player.Y, 1);
+            };
+            game.Start();
+        }
+
+        [Test]
+        public void CorrectPlayerNoMove()
+        {
+            var game = new Game(
+                new CharGameMapReader(FileFromTestsReader.GetFile("GameBoundedMapTest.txt")));
+            game.MapChanged += (sender, args) =>
+            {
+                Assert.AreEqual(args.Player.X, 1);
+                Assert.AreEqual(args.Player.Y, 1);
+            };
+            game.Start();
+            game.NewGameIteration(null, new KeyPressedArgs(ConsoleKey.LeftArrow));
+            game.NewGameIteration(null, new KeyPressedArgs(ConsoleKey.DownArrow));
+            game.NewGameIteration(null, new KeyPressedArgs(ConsoleKey.UpArrow));
+            game.NewGameIteration(null, new KeyPressedArgs(ConsoleKey.RightArrow));
+        }
+
+        [Test]
+        public void CorrectPlayerMove()
+        {
+            var game = new Game(
+                new CharGameMapReader(FileFromTestsReader.GetFile("GameFreeMapTest.txt")));
+            int[] x = {0};
+            int[] y = {0};
+            game.MapChanged += (sender, args) =>
+            {
+                Assert.AreEqual(args.Player.X, x[0]);
+                Assert.AreEqual(args.Player.Y, y[0]);
+            };
+            game.Start();
+            x[0] = 1;
+            game.NewGameIteration(null, new KeyPressedArgs(ConsoleKey.RightArrow));
+            x[0] = 0;
+            game.NewGameIteration(null, new KeyPressedArgs(ConsoleKey.LeftArrow));
+            y[0] = 1;
+            game.NewGameIteration(null, new KeyPressedArgs(ConsoleKey.DownArrow));
+            y[0] = 0;
+            game.NewGameIteration(null, new KeyPressedArgs(ConsoleKey.UpArrow));
+        }
+    }
+}
